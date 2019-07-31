ba74a0702847d9858bcdd09cfd2e5b27347596d0
tristinire9 <tristinire@gmail.com>
2019-08-01 11:43:40 +1200

    Post is working with localserver

--- commandLine.py
+++ commandLine.py
@@ -1,21 +1,22 @@
 import sys
 import requests
 from requests.exceptions import HTTPError
+def send_Function(file):
+    for url in ['http://localhost:8080']:
+        try:
+            response = requests.post(url,data={'file':open(file,'rb')})
 
-for url in ['https://api.github.com', 'http://localhost:8080']:
-    try:
-        response = requests.get(url)
+            # If the response was successful, no Exception will be raised
+            response.raise_for_status()
+        except HTTPError as http_err:
+            print(f'HTTP error occurred: {http_err}')  # Python 3.6
+        except Exception as err:
+            print(f'Other error occurred: {err}')  # Python 3.6
+        except FileNotFoundError:
+            print("FILE NOT FOUND")
+        else:
+            print('Success!')
 
-        # If the response was successful, no Exception will be raised
-        response.raise_for_status()
-    except HTTPError as http_err:
-        print(f'HTTP error occurred: {http_err}')  # Python 3.6
-    except Exception as err:
-        print(f'Other error occurred: {err}')  # Python 3.6
-    else:
-        print('Success!')
-
-print(response)
 # print ("This is the name of the script: ", sys.argv[0])
 # print ("Number of arguments: ", len(sys.argv))
 # print ("The arguments are: " , str(sys.argv))
@@ -26,3 +27,5 @@ PULL \"filename.xxx\" - Sends pull request to Store.
 
 if not len(sys.argv) > 1 or sys.argv[1].lower() not in ["push", "pull"]:
     help()
+elif sys.argv[1].lower()=="push":
+    send_Function(sys.argv[2])
