7e232e3adc9ca8a15efaf652d7cd9395a9430c2b
tristinire9 <tristinire@gmail.com>
2019-08-01 11:12:08 +1200

    Added requests

--- commandLine.py
+++ commandLine.py
@@ -1,5 +1,21 @@
 import sys
+import requests
+from requests.exceptions import HTTPError
 
+for url in ['https://api.github.com', 'http://localhost:8080']:
+    try:
+        response = requests.get(url)
+
+        # If the response was successful, no Exception will be raised
+        response.raise_for_status()
+    except HTTPError as http_err:
+        print(f'HTTP error occurred: {http_err}')  # Python 3.6
+    except Exception as err:
+        print(f'Other error occurred: {err}')  # Python 3.6
+    else:
+        print('Success!')
+
+print(response)
 # print ("This is the name of the script: ", sys.argv[0])
 # print ("Number of arguments: ", len(sys.argv))
 # print ("The arguments are: " , str(sys.argv))
@@ -8,5 +24,5 @@ def help():
 PULL \"filename.xxx\" - Sends pull request to Store.
 """)
 
-if not len(sys.argv) > 1 or sys.argv[1].lower() not in ["unzip", "create", "grade"]:
+if not len(sys.argv) > 1 or sys.argv[1].lower() not in ["push", "pull"]:
     help()
