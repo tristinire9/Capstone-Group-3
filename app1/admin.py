074d150b30ced7cb6060506dfada64ab891e6d6f
Teh Yule Kim <tehyulekim@gmail.com>
2019-08-12 06:30:53 +1200

    /admin   # with  components and software_release

--- app1/admin.py
+++ app1/admin.py
@@ -1,5 +1,37 @@
 from django.contrib import admin
 
-from .models import SoftwareRelease
+from .models import SoftwareRelease, Component
+
+
 # Register your models here.
-admin.site.register(SoftwareRelease)
+
+class ChoiceInline(admin.TabularInline):
+    model = Component
+    extra = 2
+
+
+class SoftwareReleaseAdmin(admin.ModelAdmin):
+    # fields = ['pub_date', 'product_name']
+
+    fieldsets = [
+        (None, {'fields': ['product_name']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+    list_display = ('product_name', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+
+    search_fields = ['product_name']
+
+
+class ComponentAdmin(admin.ModelAdmin):
+    fields = ['component_name', 'version']
+
+    list_display = ('component_name', 'version')
+
+    search_fields = ['component_name']
+
+
+admin.site.register(SoftwareRelease, SoftwareReleaseAdmin)
+admin.site.register(Component, ComponentAdmin)
--- app1/urls.py
+++ app1/urls.py
@@ -4,4 +4,16 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+
+    # ex: /app1/1
+    path('1', views.page1, name='page1'),
+    path('2', views.page2, name='page2'),
+
+
+    # ex: /app1/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /app1/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /app1/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
