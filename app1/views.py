8a01871aa56dd67d9b7c9b40d9baf660b067963c
tristinire9 <tristinire@gmail.com>
2019-08-12 13:04:39 +1200

    testing Django requests

--- app1/views.py
+++ app1/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("app1 index.")
+    return HttpResponse(request)
 
 
 def page1(request):
