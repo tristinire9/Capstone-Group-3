759f71effe855b038398c8e187954fb08b96cd6a
Teh Yule Kim <tehyulekim@gmail.com>
2019-08-15 10:59:17 +1200

    static html pages

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('hello.html')
+
+@app.route('/1')
+def f1():
+    return render_template('page1.html')
+@app.route('/2')
+def f2():
+    return render_template('page2.html')
+@app.route('/3')
+def f3():
+    return render_template('page3.html')
+@app.route('/4')
+def f4():
+    return render_template('page4.html')
+
 
 
 if __name__ == '__main__':
