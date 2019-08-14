7be1b8fdac083de5d65cc528ff7d86c956249dea
tristinire9 <tristinire@gmail.com>
2019-08-15 10:37:56 +1200

    added request to FLASK

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask, request, url_for, redirect
+from flask import Flask, request, url_for, redirect,jsonify
+from flask_request_params import bind_request_params
 
 app = Flask(__name__)
 
 
 @app.route('/', methods = ["GET","POST"])
 def hello_world():
-    return request.method
+    method = request.method
+    parameters = request.params
+    return request.data
 
 
 if __name__ == '__main__':
